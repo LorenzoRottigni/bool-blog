ef6b314d05ac3c929a064bc68e92394cebae8d31
LorenzoRottigni <lorenzo@rottigni.net>
2022-01-14 16:38:35 +0100

    show fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 <header class="py-5 bg-light border-bottom mb-4">
   <div class="container">
     <div class="text-center my-5">
-      <h1 class="fw-bolder">Creazione nuovo post</h1>
+      <h1 class="fw-bolder">Create new post</h1>
     </div>
   </div>
 </header>
